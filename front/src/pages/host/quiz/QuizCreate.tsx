5ca2cea6fdc5f3e9edfab73f40adb9816382b235
gksrud316 <gksrud316@naver.com>
2023-04-24 13:54:25 +0900

    feat:[FE] 퀴즈 출제 페이지 드래그 앤 드롭적용
    - 퀴즈, 게임 컴포넌트 드래그 앤 드롭 설정
    
    ~

--- front/src/pages/host/quiz/QuizCreate.tsx
+++ front/src/pages/host/quiz/QuizCreate.tsx
@@ -1,34 +1,64 @@
-import React from 'react';
+import { useState } from "react";
 import styles from "./QuizCreate.module.css"
-const QuizCreate = () => {
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd"
+const QuizCreate: React.FC = () => {
 
-    const array = ["1", "2", "3"];
+    const [itemsList, setItemsList] = useState([
+        { id: "first", content: "one" },
+        { id: "second", content: "two" },
+        { id: "third", content: "three" },
+    ]);
 
+    function onDragEnd(result: any) {
+        if (!result.destination) {
+            return;
+        }
 
+        const items = Array.from(itemsList);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
 
+        setItemsList(items);
+    }
     return (
         <div className={styles.QuizCreate}>
-
             <section className={styles.left_side} >
-                <ul className={styles.quiz_list}>
-                    {
-                        array.map(it=>{
-                            return <li>{it}</li>
-                        })
-                    }
+                <DragDropContext onDragEnd={onDragEnd}>
+                    <Droppable droppableId="one">
 
-                </ul>
+                        {(provided) => (
+                            <ul className={styles.quiz_list} ref={provided.innerRef} {...provided.droppableProps}>
+                                {itemsList.map((item, index) => (
+                                    <Draggable key={item.id} draggableId={item.id} index={index}>
+                                        {(provided) => (
+                                            <li className={styles.quiz_item}
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                            >
+
+                                                <div className={styles.quiz_title}>퀴즈 {index + 1}</div>
+                                                <div className={styles.quiz_content}>{item.content}</div>
+
+                                            </li>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {provided.placeholder}
+                                <li className={styles.quiz_add}>
+                                    <button className={styles.game_add_btn}>퀴즈추가</button>
+                                    <button className={styles.quiz_add_btn}>게임추가</button>
+                                </li>
+                            </ul>
+                        )}
+
+                    </Droppable>
+                </DragDropContext>
             </section>
 
             <section className={styles.right_side} >
                 우쪽
             </section>
-
-
-
-
-
-
         </div>
     );
 };
