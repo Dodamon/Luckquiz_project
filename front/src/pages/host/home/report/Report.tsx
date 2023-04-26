6da8c057d33b32632bb4c4aa75c8ab051fd4d968
이예진 <yjlee66096@gmail.com>
2023-04-26 17:56:28 +0900

    feat:[FE] host 홈페이지 quiz메뉴 완성

--- front/src/pages/host/home/report/Report.tsx
+++ front/src/pages/host/home/report/Report.tsx
@@ -1,7 +1,40 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import styles from "./Report.module.css";
+import { Icon } from "@iconify/react";
+import profile_sample from "assets/images/profile_sample.png";
+import report_bg from "assets/images/report_bg.png";
 
 const Report = () => {
-  return <div style={{ border: "solid 1px", borderColor: "red" }}>Report</div>;
+
+  return (
+    <div className={`${styles[`container`]}`}>
+    {/* <div className={`${styles[`container1`]}`}> */}
+    <div className={`${styles[`side`]}`}>
+      <div className={`${styles[`side-tab-box`]}`}>
+        <div className={`${styles[`profile`]}`}>
+          <img className={`${styles[`profile-img`]}`} src={profile_sample} alt="" />
+          무지개꽃잎이
+        </div>
+        <Link to={"/home"}>
+          <div className={`${styles[`menu`]}`}>
+            <Icon icon="mdi:view-list-outline" />
+            퀴즈
+          </div>
+        </Link>
+        <Link to={"/home/report"}>
+          <div className={`${styles[`menu`]}`} style={{ backgroundColor: "var(--side-bar)" }}>
+            <Icon icon="mdi:poll" />
+            레포트
+          </div>
+        </Link>
+      </div>
+    </div>
+    {/* </div> */}
+    <div className={`${styles[`content`]}`} style={{ backgroundImage: report_bg }}>
+      <div className={`${styles[`title`]}`}>레포트</div>
+    </div>
+  </div>
+  )
 };
 
 export default Report;
