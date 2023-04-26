ea92130569bc1e8ce8bb3aba5fbc27d04b2af4cb
이예은 <dodamond222@gmail.com>
2023-04-26 16:52:33 +0900

    feat:[BE] grade 서버 시작

---
+++ back/grade/src/test/java/com/luckquiz/grade/GradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckquiz.grade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
