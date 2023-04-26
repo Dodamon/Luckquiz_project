7d76999c8e3a5fdc44acccefbce4ba851cdac416
이예은 <dodamond222@gmail.com>
2023-04-26 16:57:09 +0900

    feat:[BE] quizroom 서버 시작

---
+++ back/quizroom/src/main/java/com/luckquiz/quizroom/QuizroomApplication.java
@@ -0,0 +1,13 @@
+package com.luckquiz.quizroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizroomApplication.class, args);
+	}
+
+}
