a66953b59064014b8d962a15184952085d7f0ff5
gksrud316 <gksrud316@naver.com>
2023-05-01 17:50:46 +0900

    feat:[FE] 퀴즈 템플릿 불러오기 TEST
    ~

--- front/src/pages/host/quiz/QuizSelectMenu.tsx
+++ front/src/pages/host/quiz/QuizSelectMenu.tsx
@@ -1,43 +1,32 @@
 
-import { FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from '@mui/material';
 import styles from "./QuizSelectMenu.module.css"
-import Box from '@mui/material/Box';
 import { Icon } from '@iconify/react';
-import { useState } from 'react';
-import { useDispatch } from "react-redux";
-import { authAtions } from "store/auth";
-import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from 'store';
-import { useEffect } from 'react';
 import { quizAtions } from 'store/quiz';
+import axios from "axios"
 const QuizSelectMenu = () => {
     const selectInfo = useSelector((state: RootState) => state.auth.choiceIndex);
     const quizInfo = useSelector((state: RootState) => state.quiz.quizList);
     const [selectedQuizOption, setSelectedQuizOption] = useState(quizInfo[selectInfo].quiz);
     const [selectedGameOption, setSelectedGameOption] = useState(quizInfo[selectInfo].game);
     const [selectedTimeOption, setSelectedTimeOption] = useState(quizInfo[selectInfo].timer);
-    console.log(selectInfo);
-    console.log(quizInfo);
     
     
     const dispatch = useDispatch();
-
-
     const quizTypeHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedQuizOption(event.target.value);
-        console.log(event.target.value);
         dispatch(quizAtions.quizTypeUpdate({index: selectInfo, quizType: event.target.value}))
     };
 
     const gameTypeHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedGameOption(event.target.value);
-        console.log(event.target.value);
         dispatch(quizAtions.gameTypeUpdate({index: selectInfo, gameType: event.target.value}))
     };
 
     const quizTimeHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedTimeOption(parseInt(event.target.value));
-        console.log(event.target.value);
         dispatch(quizAtions.quizTimeUpdate({index: selectInfo, time: event.target.value}))
     };
 
@@ -51,13 +40,19 @@ const QuizSelectMenu = () => {
         }else if(quizInfo[selectInfo]?.quizType==="퀴즈"){
             setSelectedQuizOption(quizInfo[selectInfo].quiz);
             setSelectedTimeOption(quizInfo[selectInfo].timer);
-            // dispatch(quizAtions.gameTypeUpdate({index: selectInfo, quizType: selectedQuizOption}))
-            // dispatch(quizAtions.quizTimeUpdate({index: selectInfo, time: selectedTimeOption}))
         }
       
     }, [quizInfo, selectInfo])
 
+    const temporarySaveHandler = ()=>{
+        
+        axios.get("https://k8a707.p.ssafy.io/api/quiz/template/info?templateId=1&hostId=1").then(res=>{
+            console.log(res);
+            
+        })
 
+        
+    }
 
 
 
@@ -89,7 +84,7 @@ const QuizSelectMenu = () => {
 
             <div className={styles.nav_right}>
                 <div>
-                    <div>임시저장</div>
+                    <div onClick={temporarySaveHandler}>임시저장</div>
                 </div>
                 <div>
                     <div>저장</div><Icon icon="ic:round-log-out" />
