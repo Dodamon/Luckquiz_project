10fd98b7661dbec802292a8a6f8fc38a50f40234
이예진 <yjlee66096@gmail.com>
2023-05-02 21:17:00 +0900

    fix:[FE] podium 반응형 수정
    - 반응형 %로 수정

--- front/src/components/common/Podium.tsx
+++ front/src/components/common/Podium.tsx
@@ -28,20 +28,20 @@ const Podium = () => {
     // 시상대에 맞게 순위별 프로필 랜더링
     setTimeout(() => {
       setShowFirstProfile(true);
-    }, 985);
+    }, 990);
     setTimeout(() => {
       setShowSecondProfile(true);
-    }, 1410);
+    }, 1415);
     setTimeout(() => {
       setShowThirdProfile(true);
-    }, 1990);
+    }, 1985);
 
     setTimeout(() => {
       confetti({
-        particleCount: 200,
-        spread: 100,
+        particleCount: 250,
+        spread: 200,
         origin: {
-          x: 0.5,
+          x: 0.6,
           // since they fall down, start a bit higher than random
           y: 0.85,
         },
@@ -60,21 +60,21 @@ const Podium = () => {
   return (
     <div className={styles.container}>
       {showFirstProfile && (
-        <div className={styles.profileBox} style={{ position: "absolute", left: 245, top: 30 }}>
+        <div className={styles.profileBox} style={{ position: "absolute", left: "31%", bottom: "56.5%" }}>
           <img src={profile1} alt="" className={styles.profileImg} />
-          <div className={styles.nameTag}></div>
+          <div className={styles.nameTag}>무지개꽃잎이</div>
         </div>
       )}
       {showSecondProfile && (
-        <div className={styles.profileBox} style={{ position: "absolute", left: 30, top: 72 }}>
+        <div className={styles.profileBox} style={{ position: "absolute", left: "-4%", bottom: "45%" }}>
           <img src={profile2} alt="" className={styles.profileImg} />
-          <div className={styles.nameTag}></div>
+          <div className={styles.nameTag}>무지개꽃잎이</div>
         </div>
       )}
       {showThirdProfile && (
-        <div className={styles.profileBox} style={{ position: "absolute", left: 465, top: 105 }}>
+        <div className={styles.profileBox} style={{ position: "absolute", left: "66%", bottom: "35.5%" }}>
           <img src={profile3} alt="" className={styles.profileImg} />
-          <div className={styles.nameTag}></div>
+          <div className={styles.nameTag}>무지개꽃잎이</div>
         </div>
       )}
       <div ref={aniBox}></div>
