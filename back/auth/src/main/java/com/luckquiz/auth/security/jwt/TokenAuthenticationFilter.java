e5aefdf47fec653ffabe2962fd6a2e851544f148
이예은 <dodamond222@gmail.com>
2023-05-12 00:59:36 +0900

    fix:[BE] 토큰 필터 수정

--- back/auth/src/main/java/com/luckquiz/auth/security/jwt/TokenAuthenticationFilter.java
+++ back/auth/src/main/java/com/luckquiz/auth/security/jwt/TokenAuthenticationFilter.java
@@ -1,8 +1,13 @@
 package com.luckquiz.auth.security.jwt;
 
 import com.luckquiz.auth.security.service.CustomUserDetailsService;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,6 +21,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.Key;
 import java.util.UUID;
 
 @Slf4j
@@ -25,6 +31,16 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
     private final TokenProvider tokenProvider;
     private final CustomUserDetailsService customUserDetailsService;
+
+    @Value("${app.auth.token-secret}")
+    private String SECRET_KEY;
+    private Key key;
+
+    @Override
+    public void afterPropertiesSet() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -38,19 +54,48 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
                 return;
             }
 
-            tokenProvider.validateToken(accesstoken);
-            log.info("토큰을 세션에 올립니다");
-            UUID userId = tokenProvider.getUserIdFromToken(accesstoken);
-            UserDetails userDetails = customUserDetailsService.loadUserById(userId);
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(key).parseClaimsJws(accesstoken);
 
+        } catch (SignatureException ex) {
+            log.error("Unsupported Signature");
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+            return;
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+            return;
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token");
+            // access token이 만료됐을 때
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
+            return;
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token");
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+            return;
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty.");
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+            return;
         } catch (Exception ex) {
-            logger.error("Could not set user authentication in security context", ex);
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰이 이상합니다..");
+            log.error(ex.toString());
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, ex.getMessage());
             return;
         }
+
+//            tokenProvider.validateToken(accesstoken);
+//            log.info("토큰을 세션에 올립니다");
+//            UUID userId = tokenProvider.getUserIdFromToken(accesstoken);
+//            UserDetails userDetails = customUserDetailsService.loadUserById(userId);
+//            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+//            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//
+//        } catch (Exception ex) {
+//            logger.error("Could not set user authentication in security context", ex);
+//            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰이 이상합니다..");
+//            return;
+//        }
         filterChain.doFilter(request, response);
     }
 
