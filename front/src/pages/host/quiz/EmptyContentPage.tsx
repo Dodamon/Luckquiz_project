4598d27591ece1401c601cf9eeff6218edce6140
yxxddii <syooj0728@gmail.com>
2023-05-17 04:28:30 +0900

    css:[FE] Fix css

--- front/src/pages/host/quiz/EmptyContentPage.tsx
+++ front/src/pages/host/quiz/EmptyContentPage.tsx
@@ -12,12 +12,13 @@ const EmptyContentPage = () => {
             <div className={styles.exitbox}>
                 <div className={styles.exit_btn} >
                     <div className={styles.btn_comment} onClick={()=>  navigate("/home",{replace: true})}>나가기</div>
+                    
                 </div>
             </div>
 
             <div className={styles.emptybox} >
                 <div className={styles.logo}><img src={logo} alt="로고" /></div>
-                <div className={styles.comment}>  컨텐츠가 없습니다.퀴즈 혹은 게임을 추가해주세요</div>
+                <div className={styles.comment}>  컨텐츠가 없습니다. 퀴즈 혹은 게임을 추가해주세요</div>
             </div>
         </div >
     );
--- front/src/pages/host/quiz/GameSelection.tsx
+++ front/src/pages/host/quiz/GameSelection.tsx
@@ -26,7 +26,7 @@ const GameSelection = () => {
 
                 <ul className={styles.game_list}>
                     <li className={styles.game_item} onClick={() => gameSelectHandler("emotion")}>
-                        <header className={styles.game_title}><h4>셀카 감정분석게임</h4></header>
+                        <header className={styles.game_title}>셀카 감정분석게임</header>
                         <section className={styles.game_img} ><img src={emotion} alt="감정분석" /></section>
                         <footer className={styles.game_setting}><div className={styles.game_btnbox}>
                             <div className={styles.game_btn} style={{ backgroundColor: "var( --button-two)" }}><Icon icon="akar-icons:triangle-right" /></div>
@@ -36,7 +36,7 @@ const GameSelection = () => {
                     </li>
 
                     <li className={styles.game_item} onClick={() => gameSelectHandler("wakeup")}>
-                        <header className={styles.game_title} ><h4>일어나 럭퀴야 학교가자</h4></header>
+                        <header className={styles.game_title} >일어나 럭퀴야 학교가자</header>
                         <section className={styles.game_img} ><img src={wakeup} alt="감정분석" /></section>
                         <footer className={styles.game_setting}><div className={styles.game_btnbox}>
                             <div className={styles.game_btn} style={{ backgroundColor: "var( --button-two)" }}><Icon icon="akar-icons:triangle-right" /></div>
@@ -46,7 +46,7 @@ const GameSelection = () => {
                     </li>
 
                     <li className={styles.game_item} onClick={() => gameSelectHandler("balloon")}>
-                        <header className={styles.game_title}><h4>풍선 터뜨리기</h4></header>
+                        <header className={styles.game_title}>풍선 터뜨리기</header>
                         <section className={styles.game_img} ><img src={pop} alt="감정분석" /></section>
                         <footer className={styles.game_setting}><div className={styles.game_btnbox}>
                             <div className={styles.game_btn} style={{ backgroundColor: "var( --button-two)" }}><Icon icon="akar-icons:triangle-right" /></div>
@@ -60,7 +60,7 @@ const GameSelection = () => {
                     <li className={styles.game_item_etc}>
 
 
-                        <header className={styles.game_title}><h4>퀴즈 준비중</h4></header>
+                        <header className={styles.game_title}><h5>퀴즈 준비중</h5></header>
                         <section className={styles.game_mid} >
                             <Icon icon="ic:baseline-question-mark" />
                         </section>
