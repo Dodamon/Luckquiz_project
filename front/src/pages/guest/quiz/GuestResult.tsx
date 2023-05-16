d632c0e7f016909905f4f41636fdb47f5980b427
이예진 <yjlee66096@gmail.com>
2023-05-16 14:34:18 +0900

    feat: [FE] guestresult 주석삭제
    - guestresult 주석삭제

--- front/src/pages/guest/quiz/GuestResult.tsx
+++ front/src/pages/guest/quiz/GuestResult.tsx
@@ -12,23 +12,13 @@ const GuestResult = () => {
   const quizItem = useSelector((state: RootState) => state.socket.quizItem);
   const result = useSelector((state: RootState) => state.socket.getGuestResult)
   const finalResult = useSelector((state: RootState) => state.socket.getFinalResultList)
-  // const [blockMount, setBlockMount] = useState(0); // 최초 마운트시 콜백실행을 막기 위한 변수
-  
-  // const deleteAndNavigate = () => {
-  //   dispatch(socketActions.deleteGuestResult).then(() => {
-  //     navigate(`/guest/quiz/play`)
-  //   })
-  // }
   
   // 새로운 퀴즈가 들어오면 퀴즈페이지로 이동
   useEffect(() => {
     console.log("게스트가 받은 새퀴즈:", quizItem);
-    // console.log(blockMount);
     if (quizItem?.quizNum !== result?.quizNum) {
-      // deleteAndNavigate() // 다음 퀴즈진행창에서 마운트시 바로 넘어오는것을 막기위해 현재 result 삭제
       navigate('/guest/quiz/play')
       }
-    // setBlockMount((prev) => prev + 1); // 최초 실행을 막기 위한 함수
   }, [quizItem]);
 
   // 최종결과가 들어오면 어워즈페이지로 이동
