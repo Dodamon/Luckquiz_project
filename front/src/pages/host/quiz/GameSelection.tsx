ac77e608288e38034c7f048547870d6938307005
gksrud316 <gksrud316@naver.com>
2023-05-17 17:47:50 +0900

    css:[FE] 게임 셀렉트 페이지 CSS 작업
    ~

--- front/src/pages/host/quiz/GameSelection.tsx
+++ front/src/pages/host/quiz/GameSelection.tsx
@@ -9,11 +9,13 @@ import { useSelector } from 'react-redux';
 import { RootState } from 'store';
 import { useDispatch } from 'react-redux';
 import { quizAtions } from 'store/quiz';
+import { useNavigate } from 'react-router';
+
 
 const GameSelection = () => {
     const selectInfo = useSelector((state: RootState) => state.auth.choiceIndex);
     const dispatch = useDispatch();
-
+    const navigate = useNavigate();
     const gameSelectHandler = (gameType: string) => {
         dispatch(quizAtions.gameTypeUpdate({ index: selectInfo, gameType: gameType }));
     }
@@ -24,54 +26,51 @@ const GameSelection = () => {
         <>
             <div className={styles.game_content}>
 
+                <div className={styles.main_title}>
+                    <div className={styles.main_ment}>GAME WORLD</div>
+                 
+                    </div>
+
                 <ul className={styles.game_list}>
-                    <li className={styles.game_item} onClick={() => gameSelectHandler("emotion")}>
+                    <li className={styles.game_item} style={{backgroundColor:"var(--select-four)"}} >
                         <header className={styles.game_title}>셀카 감정분석게임</header>
-                        <section className={styles.game_img} ><img src={emotion} alt="감정분석" /></section>
+                        <section className={styles.game_img} ><img src={emotion} alt="감정분석" style={{objectFit:"fill"}} /></section>
                         <footer className={styles.game_setting}><div className={styles.game_btnbox}>
-                            <div className={styles.game_btn} style={{ backgroundColor: "var( --button-two)" }}><Icon icon="akar-icons:triangle-right" /></div>
-                            <div className={styles.game_btn} style={{ backgroundColor: "var( --button-one)" }}><Icon icon="ic:round-plus" /></div>
+                            {/* <div className={styles.game_btn} style={{ backgroundColor: "var( --button-two)" }}><Icon icon="akar-icons:triangle-right" /></div> */}
+                            <div className={styles.game_btn} style={{ backgroundColor: "skyblue" }} onClick={() => gameSelectHandler("emotion")}><Icon icon="ic:round-plus" /></div>
                         </div>
                         </footer>
                     </li>
 
-                    <li className={styles.game_item} onClick={() => gameSelectHandler("wakeup")}>
+                    <li className={styles.game_item} style={{backgroundColor:"var(--select-three)"}} >
                         <header className={styles.game_title} >일어나 럭퀴야 학교가자</header>
-                        <section className={styles.game_img} ><img src={wakeup} alt="감정분석" /></section>
+                        <section className={styles.game_img} ><img src={wakeup} alt="감정분석"  style={{objectFit:"fill"}}/></section>
                         <footer className={styles.game_setting}><div className={styles.game_btnbox}>
-                            <div className={styles.game_btn} style={{ backgroundColor: "var( --button-two)" }}><Icon icon="akar-icons:triangle-right" /></div>
-                            <div className={styles.game_btn} style={{ backgroundColor: "var( --button-one)" }}><Icon icon="ic:round-plus" /></div>
+                            {/* <div className={styles.game_btn} style={{ backgroundColor: "var( --button-two)" }}><Icon icon="akar-icons:triangle-right" /></div> */}
+                            <div className={styles.game_btn} style={{ backgroundColor: "var(--select-one" }} onClick={() => gameSelectHandler("wakeup")}><Icon icon="ic:round-plus" /></div>
                         </div>
                         </footer>
                     </li>
 
-                    <li className={styles.game_item} onClick={() => gameSelectHandler("balloon")}>
+                    <li className={styles.game_item} style={{backgroundColor:"var(--select-one)"}} >
                         <header className={styles.game_title}>풍선 터뜨리기</header>
-                        <section className={styles.game_img} ><img src={pop} alt="감정분석" /></section>
+                        <section className={styles.game_img} ><img src={pop} alt="감정분석" style={{objectFit:"fill"}} /></section>
                         <footer className={styles.game_setting}><div className={styles.game_btnbox}>
-                            <div className={styles.game_btn} style={{ backgroundColor: "var( --button-two)" }}><Icon icon="akar-icons:triangle-right" /></div>
-                            <div className={styles.game_btn} style={{ backgroundColor: "var( --button-one)" }}><Icon icon="ic:round-plus" /></div>
+                            {/* <div className={styles.game_btn} style={{ backgroundColor: "var( --button-two)" }}><Icon icon="akar-icons:triangle-right" /></div> */}
+                            <div className={styles.game_btn} style={{ backgroundColor: "var(--select-three)" }} onClick={() => gameSelectHandler("balloon")}><Icon icon="ic:round-plus" /></div>
                         </div>
                         </footer>
                     </li>
                 </ul>
 
-                <ul className={styles.game_list}>
-                    <li className={styles.game_item_etc}>
+     
 
-
-                        <header className={styles.game_title}><h5>퀴즈 준비중</h5></header>
-                        <section className={styles.game_mid} >
-                            <Icon icon="ic:baseline-question-mark" />
-                        </section>
-
-
-                    </li>
-
-
-                </ul>
-
-
+                <div className={styles.exitbox}>
+                <div className={styles.exit_btn} >
+                    <div className={styles.btn_comment} onClick={()=>  navigate("/home",{replace: true})}>나가기</div>
+                    
+                </div>
+                </div>
 
             </div>
         </>
