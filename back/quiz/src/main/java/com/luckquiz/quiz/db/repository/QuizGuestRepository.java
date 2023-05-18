22deb4a6716764834c10b0a010d1b363b2d8edb3
RyeoHwan <arh951019@gmail.com>
2023-05-18 13:22:50 +0900

    [BE] feat: update report info

--- back/quiz/src/main/java/com/luckquiz/quiz/db/repository/QuizGuestRepository.java
+++ back/quiz/src/main/java/com/luckquiz/quiz/db/repository/QuizGuestRepository.java
@@ -9,6 +9,7 @@ public interface QuizGuestRepository extends JpaRepository<QuizGuest, Integer> {
     Optional<QuizGuest> findByTemplateId(Integer templateId);
     Optional<QuizGuest> findQuizGuestByPinNum(Integer pinNum);
 
+    boolean existsByGuestNickname(String guestNickName);
     Optional<QuizGuest> findQuizGuestByGuestNicknameAndQuizRoomId(String guestNickname,int quizRoomId);
 }
 
