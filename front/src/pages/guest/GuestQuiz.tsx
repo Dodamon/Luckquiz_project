7ab6f0d35897205cec6c3eb164832418e55d66fb
gksrud316 <gksrud316@naver.com>
2023-05-18 17:31:31 +0900

    feat:[FE] 호스트, 게스트 새로고침/나가기 확인창 설정
    ~

--- front/src/pages/guest/GuestQuiz.tsx
+++ front/src/pages/guest/GuestQuiz.tsx
@@ -1,7 +1,42 @@
 import { Outlet } from "react-router-dom";
 import styles from "./GuestQuiz.module.css";
+import { useEffect } from "react";
 
 const GuestQuiz = () => {
+
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      event.preventDefault();
+      event.returnValue = ""; // Chrome에서는 이 설정이 필요합니다.
+    };
+
+    const handlePopstate = (event: PopStateEvent) => {
+      const shouldLeaveGame = window.confirm("게임에서 나가시겠습니까?");
+      if (shouldLeaveGame) {
+        // 게임을 나가는 로직을 여기에 추가하세요.
+        // 예를 들어, 게임 상태를 초기화하거나 서버에 나가는 요청을 보낼 수 있습니다.
+        // ...
+
+        // 페이지를 벗어납니다.
+        
+      } else {
+        // 뒤로가기 동작을 취소합니다.
+        window.history.pushState(null, "", window.location.pathname);
+      }
+    };
+
+    // 브라우저 닫기 이벤트 처리
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    // 뒤로가기 버튼 이벤트 처리
+    window.addEventListener("popstate", handlePopstate);
+
+    return () => {
+      // 이벤트 리스너를 정리합니다.
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      window.removeEventListener("popstate", handlePopstate);
+    };
+  }, []);
   return (
     <div className={styles.guestQuizContainer}>
       <Outlet></Outlet>
